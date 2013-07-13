77a0145733b41692cb3b661bcd53640a1a5e1a9f
toshiwo <toshiwo@toshiwo.com>
2013-07-13 14:48:06 +0900

    update togglable_section

--- togglable_section/table_view_module/togglable_section.rb
+++ togglable_section/table_view_module/togglable_section.rb
@@ -1,13 +1,40 @@
 module TableViewModule
   module TogglableSection
 
+    def toggle_section index
+      @section_statuses ||= build_section_statuses
+
+      @section_statuses[index] = !!!@section_statuses[index]
+    end
+
     def tableView table_view, viewForHeaderInSection:section
-      return TogglableTableHeaderView.new
+      @section_view ||= []
+
+      if @section_view[section]
+        view = @section_view[section]
+      else
+        view = TogglableTableHeaderView.new
+        view.tableView = table_view
+
+        @section_view[section] = view
+      end
+
+      return view
     end
 
     def tableView table_view, heightForRowAtIndexPath:index_path
-      0.0
+      @section_statuses ||= build_section_statuses
+
+      @section_statuses[index_path.section] == true ? 43.0 : 0.0
     end
+
+    def build_section_statuses
+      self.table_data.map do |section|
+        # TODO: current vertion default: false (hidden)
+        section[:title].nil? ? nil : false
+      end
+    end
+    private :build_section_statuses
 
   end
 end
--- togglable_section/views/togglable_table_header_view.rb
+++ togglable_section/views/togglable_table_header_view.rb
@@ -1,6 +1,27 @@
 class TogglableTableHeaderView < UITableViewHeaderFooterView
 
   def touchesEnded touches, withEvent:event
+    touch = touches.allObjects.first
+    point = touch.locationInView self.tableView
+
+    if index = detect_touch_section(point)
+      self.tableView.delegate.toggle_section index
+      self.tableView.reloadData
+    end
   end
+
+  def detect_touch_section point
+    section_size = self.tableView.dataSource.numberOfSectionsInTableView self.tableView
+
+    section_size.times do |i|
+      index = (section_size - i - 1)
+      rect = self.tableView.rectForHeaderInSection index
+
+      return index if CGRectContainsPoint rect, point
+    end
+
+    nil
+  end
+  private :detect_touch_section
 
 end
